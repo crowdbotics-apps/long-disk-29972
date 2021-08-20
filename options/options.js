43f073cb8c617d70f54c79c848cb94ff02818069
Crowdbotics <team@crowdbotics.com>
2021-08-20 12:42:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "long_disk_29972Identifier",
+  url: "https://long_disk_29972Identifier.botics.co",
+  api: "https://long_disk_29972Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
